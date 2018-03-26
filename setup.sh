913381caccd48438c9db0027b7f17d73f155e4e2
David Zhang <zyq.david@gmail.com>
2018-03-26 18:13:54 -0400

    2018-03-26 updates

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 # enable hold movement in vim
 defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write -g InitialKeyRepeat -int 15
+defaults write -g KeyRepeat -int 2
 
 # setup bash profile
 ln -s settings/bash_profile ~/.bash_profile
 ln -s settings/git-prompt.sh ~/.git-prompt.sh
+ln -s settings/gitignore ~/.gitignore
+
 . ~/.bash_profile
-ln -s settings/global_gitignore ~/.gitignore
 
 git config --global core.excludesfile ~/.gitignore

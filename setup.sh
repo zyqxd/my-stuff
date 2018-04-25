275e24aa995be0bb804d231e7911d52528a18e1a
David Zhang <zyq.david@gmail.com>
2018-04-25 11:36:25 -0400

    more changes

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
 
 # setup bash profile
-ln -s settings/bash_profile ~/.bash_profile
-ln -s settings/git-prompt.sh ~/.git-prompt.sh
-ln -s settings/gitignore ~/.gitignore
+ln -s -i settings/bash_profile ~/.bash_profile
+ln -s -i settings/git-prompt.sh ~/.git-prompt.sh
+ln -s -i settings/gitignore ~/.gitignore
 
 . ~/.bash_profile
 

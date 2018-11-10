185fabe5e1b14d6d148ffa41f82a9414c7881909
David Zhang <zyq.david@gmail.com>
2018-11-10 22:07:27 +0800

    repo name change

--- setup.sh
+++ setup.sh
@@ -4,15 +4,15 @@ defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
 
 # setup bash profile
-ln -sf ~/Workspace/Tools/preferences/bash/bash_profile ~/.bash_profile
-ln -sf ~/Workspace/Tools/preferences/bash/git-prompt.sh ~/.git-prompt.sh
+ln -sf ~/Workspace/my-stuff/preferences/bash/bash_profile ~/.bash_profile
+ln -sf ~/Workspace/my-stuff/preferences/bash/git-prompt.sh ~/.git-prompt.sh
 
 # setup git
-ln -sf ~/Workspace/Tools/preferences/gitignore ~/.gitignore
+ln -sf ~/Workspace/my-stuff/preferences/gitignore ~/.gitignore
 
 # Setup vscode
-ln -sf ~/Workspace/Tools/preferences/vscode/vscode.css ~/.vscode.css
-ln -sf ~/Workspace/Tools/preferences/vscode/vscode.json $HOME/Library/Application\ Support/Code/User/settings.json
+ln -sf ~/Workspace/my-stuff/preferences/vscode/vscode.css ~/.vscode.css
+ln -sf ~/Workspace/my-stuff/preferences/vscode/vscode.json $HOME/Library/Application\ Support/Code/User/settings.json
 
 source ~/.bash_profile
 
